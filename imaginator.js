5fdbe01e363c131d84830f8b944d75694eefdf53
MakerSquare Student <students@themakersquare.com>
2013-07-16 15:18:08 -0500

    Added crossfade and captions

--- imaginator.js
+++ imaginator.js
@@ -2,13 +2,14 @@ $(document).ready(function() {
 	$('img').first().addClass('active');
 	firstImageClone = $('img').first().clone();
 	firstImageClone.removeAttr('width');
-	firstImageClone.removeAttr('data-caption');
 	$('#panel').append(firstImageClone);
+	$('#panel').prepend("<div id=caption>" + $('img').first().attr('data-caption') + "</div>");
+
 
 	function newPanelFadeIn(clickedImage) {
 		newPanelImage = $(clickedImage).clone();
-		newPanelImage.removeAttr('width data-caption');
-		$('#panel').append(newPanelImage.fadeIn(4000));
+		newPanelImage.removeAttr('width');
+		$('#panel').append(newPanelImage.fadeIn(500));
 	};
 
 	$('#sidebar img').click(function(){
@@ -20,18 +21,12 @@ $(document).ready(function() {
   		// 	newPanelFadeIn(clickedImage);
   		// });
 		newPanelFadeIn(clickedImage);
-		
-
-		$('#panel img').first().fadeOut(3000, function(){
+		$('#panel img').first().fadeOut(500, function(){
 			$('#panel img').first().remove();
 		});
-
-  		
-  		
-  	
-
-	});
-
-	
+		newCaption = $(this).attr('data-caption');
+		$('#caption').first().remove();
+		$('#panel').prepend("<div id=caption>" + newCaption + "</div>");
+	});	
 });
 
